d2ab0debc5d96b90c4b5d72f17dd26c1dec49cba
Tyler Parker <tparker@computemachines.com>
2018-10-25 18:39:26 -0400

    Rename a placeholder component.

--- src/client/entry.js
+++ src/client/entry.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from '../components/App'
-import Message from '../components/Message'
 
 // ReactDom.hydrate(App, document.getElementById('react-root'))
-
 ReactDOM.hydrate(<App />, document.getElementById('react-root'))
