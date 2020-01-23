add0739b0f2a35c0c91c300c67e4d7ed729cfe87
Tyler Parker <tparker@computemachines.com>
2020-01-23 10:49:22 -0500

    Following front end masters, react intro v5.

--- webpack.client.production.config.js
+++ webpack.client.production.config.js
@@ -2,10 +2,9 @@ const merge = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.client.common.js");
 
-
 module.exports = merge(common, {
   mode: "production",
-  
+
   optimization: {
     minimizer: [new TerserPlugin()]
   }
