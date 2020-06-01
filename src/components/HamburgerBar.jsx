a121fcb2051770f4961d4e7c473da4197a19e3b3
Tyler Parker <tparker@computemachines.com>
2020-06-01 13:23:42 -0400

    Convert HamburgerBar to use hooks

--- src/components/HamburgerBar.jsx
+++ src/components/HamburgerBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "./Navbar";
 
@@ -14,25 +14,18 @@ const HamburgerButton = ({ onClick, show }) => (
   </button>
 );
 
-export default class HamburgerBar extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = { show: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState((state) => ({ show: !state.show }));
-  }
-  render() {
-    return (
-      <div className="fixed-bar">
-        <div className="branding">
-          <div className="logo" />
-          <h2>Inventory App</h2>
-          <HamburgerButton onClick={this.handleClick} show={this.state.show} />
-        </div>
-        <Navbar show={this.state.show} />
+const HamburgerBar = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="fixed-bar">
+      <div className="branding">
+        <div className="logo" />
+        <h2>Inventory App</h2>
+        <HamburgerButton onClick={() => setShow(!show)} show={show} />
       </div>
-    );
-  }
-}
+      <Navbar show={show} />
+    </div>
+  );
+};
+
+export default HamburgerBar;
