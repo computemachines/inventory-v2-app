a26c4a105c0c231bf7d5bea21746f1fd25cb1594
Tyler Parker <tparker@computemachines.com>
2020-06-22 19:07:18 -0400

    Add styling for labels

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 
-import UnitPreview from "./UnitPreview";
+import Bin from "./Bin";
+import Sku from "./Sku";
+import Batch from "./Batch";
+import Uniq from "./Uniq";
+
+const Unit = ({ json, preview }) => {
+  console.log(json);
+  if (json.id.startsWith("BIN")) {
+    return <Bin {...{ preview }} bin_id={json.id} />;
+  }
+  if (json.id.startsWith("SKU")) {
+    return <Sku {...{ preview }} json={json} />;
+  }
+  if (json.id.startsWith("BAT")) {
+    return <Batch {...{ preview }} json={json} />;
+  }
+  if (json.id.startsWith("UNIQ")) {
+    return <Uniq {...{ preview }} json={json} />;
+  }
+  return <p>BAD</p>;
+};
 
 const SearchResults = ({ results }) => {
   return (
     <ul className="search-results">
       {results.map((result) => (
-        <UnitPreview key={result.id} json={result} />
+        <Unit preview key={result.id} json={result} />
       ))}
     </ul>
   );
