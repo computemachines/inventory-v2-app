c621bb81262d34d6bb3976895c7939733b1038f8
Tyler Parker <tparker@computemachines.com>
2020-07-03 15:19:59 -0400

    Pager links now scroll to top by default.

--- src/components/Pager.jsx
+++ src/components/Pager.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { Link } from "@reach/router";
-export function Pager({ currentPage: page, numPages, linkHref }) {
+export function Pager({
+  currentPage: page,
+  numPages,
+  linkHref,
+  scrollToTop = true,
+}) {
+  var onClick;
   const shownPageLinks = [page - 2, page - 1, page, page + 1, page + 2].filter(
     (p) => p >= 1 && p <= numPages
   );
 
   if (!numPages) return null;
 
+  if (scrollToTop) {
+    onClick = () => window.scrollTo(0, 0);
+  } else {
+    onClick = () => {};
+  }
+
   return (
     <div className="pager">
       <span className="pager--note">Page:</span>
       {page - 2 > 1 ? (
-        <Link to={linkHref + 1} className="pager--page-link">
+        <Link onClick={onClick} to={linkHref + 1} className="pager--page-link">
           |&lt;
         </Link>
       ) : null}
       {page !== 1 ? (
-        <Link to={linkHref + (page - 1)} className="pager--page-link">
+        <Link
+          onClick={onClick}
+          to={linkHref + (page - 1)}
+          className="pager--page-link"
+        >
           &lt;
         </Link>
       ) : null}
       {page - 2 > 1 ? "..." : null}
       {shownPageLinks.map((p) => (
         <Link
+          onClick={onClick}
           to={linkHref + p}
           key={p}
           className={`pager--page-link ${
@@ -34,12 +51,20 @@ export function Pager({ currentPage: page, numPages, linkHref }) {
       ))}
       {page + 2 < numPages ? "..." : null}
       {numPages > page ? (
-        <Link to={linkHref + (page + 1)} className="pager--page-link">
+        <Link
+          onClick={onClick}
+          to={linkHref + (page + 1)}
+          className="pager--page-link"
+        >
           &gt;
         </Link>
       ) : null}
       {page + 2 < numPages ? (
-        <Link to={linkHref + numPages} className="pager--page-link">
+        <Link
+          onClick={onClick}
+          to={linkHref + numPages}
+          className="pager--page-link"
+        >
           &gt;|
         </Link>
       ) : null}
