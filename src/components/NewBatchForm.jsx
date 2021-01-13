7b8c9256fbf45780df65956bfa1d9b58888dbc68
Tyler Parker <tparker@computemachines.com>
2021-01-13 16:13:14 -0500

    Add code to post batch data from form

--- src/components/NewBatchForm.jsx
+++ src/components/NewBatchForm.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
-import actions from "../actions";
+// /* eslint-disable */
+// import actions from "../actions";
 import { printLabel } from "../local_utils";
 
-const NewBatchForm = (nextBatchId, setNextBatchId) => {
+const NewBatchForm = ({ nextBatchId, setNextBatchId }) => {
   // const [alert, setAlert] = useState(null);
   const [batchId, setBatchId] = useState(nextBatchId);
-  if (0 == batchId) {
-    setNextBatchId(0);
-  }
+  const [name, setName] = useState("");
+  const [skuId, setSkuId] = useState("");
+  const [ownedCodes, setOwnedCodes] = useState("");
+  const [assocCodes, setAssocCodes] = useState("");
+
+  useEffect(() => {
+    fetch("/api/next/batch")
+      .then((resp) => resp.json())
+      .then((data) => setNextBatchId(data.state));
+  }, [setNextBatchId]);
+
   return (
     <div>
       <form
@@ -17,10 +26,21 @@ const NewBatchForm = (nextBatchId, setNextBatchId) => {
           fetch("/api/batches", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({}),
+            body: JSON.stringify({
+              id: batchId || nextBatchId,
+              name,
+              sku_id: skuId,
+              owned_codes: ownedCodes,
+              associated_codes: assocCodes,
+            }),
           })
-            .then((resp) => resp.json())
-            .then(console.log);
+            .then(console.log)
+            .then(() => {
+              console.log("fetching next batch id");
+              fetch("/api/next/batch")
+                .then((resp) => resp.json())
+                .then((data) => setNextBatchId(data.state));
+            });
           e.preventDefault();
         }}
       >
@@ -31,18 +51,64 @@ const NewBatchForm = (nextBatchId, setNextBatchId) => {
               type="text"
               className="form-input"
               id="batch_id"
-              required
               value={batchId}
+              placeholder={nextBatchId}
               onChange={(e) => setBatchId(e.target.value)}
             />
             <button
               type="button"
               className="form-input-print-button"
-              onClick={() => printLabel(batchId)}
+              onClick={() => printLabel(batchId || nextBatchId)}
             >
               Print
             </button>
           </div>
+        </label>
+        <label htmlFor="sku_id" className="form-label">
+          SKU Label
+          <input
+            type="text"
+            name="sku_id"
+            id="sku_id"
+            className="form-input"
+            value={skuId}
+            onChange={(e) => setSkuId(e.target.value)}
+          />
+        </label>
+        <label htmlFor="name" className="form-label">
+          Name
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="owned_codes" className="form-label">
+          Owned Codes
+          <textarea
+            name="owned_codes"
+            id="owned_codes"
+            // cols="30"
+            // rows="10"
+            className="form-input"
+            value={ownedCodes}
+            onChange={(e) => setOwnedCodes(e.target.value)}
+          />
+        </label>
+        <label htmlFor="assoc_codes" className="form-label">
+          Associated Codes
+          <textarea
+            name="assoc_codes"
+            id="assoc_codes"
+            // cols="30"
+            // rows="10"
+            className="form-input"
+            value={assocCodes}
+            onChange={(e) => setAssocCodes(e.target.value)}
+          ></textarea>
         </label>
         <input type="submit" value="Submit" className="form-submit" />
       </form>
@@ -56,7 +122,10 @@ const mapStateToProps = (storeState) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   setNextBatchId(id) {
-    dispatch(actions.setBatchId(id));
+    dispatch({
+      type: "SET_NEXT_BATCH",
+      payload: id,
+    });
   },
 });
 
